8d08f8efec8f398221b3b0cb1f8120d0268f7e64
Cyrano75 <berthierjg@gmail.com>
2024-12-12 21:52:20 +0100

    fix(debug): add debug logs for scope

--- src/pages/api/slack/auth.tsx
+++ src/pages/api/slack/auth.tsx
@@ -2,6 +2,7 @@ import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
     const scopes = "channels:read,channels:history,groups:read,im:read,mpim:read";
+    console.log("Redirecting to Slack OAuth with scopes:", scopes);
     res.redirect(
       `https://slack.com/oauth/v2/authorize?client_id=${
         process.env.SLACK_CLIENT_ID
